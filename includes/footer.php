95e91c4a0bded87b949bdff05abb03431536f91f
blackmamba279 <Oscarrod54@gmail.com>
2025-04-17 19:26:39 -0600

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> BoutiqueMG Reservas. Todos los derechos reservados 2025.</p>
+        </div>
+    </footer>
+    <script src="/assets/js/main.js"></script>
+</body>
+</html>
